4418460df5beb6c4d3084f65ccf609200895c65e
armyscoutrick <armyscoutrick@yahoo.com>
2025-02-26 19:05:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from stats import get_num_words
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
@@ -17,9 +18,7 @@ def main():
     print("--- End report ---")
 
 
-def get_num_words(text):
-    words = text.split()
-    return len(words)
+
 
 
 def sort_on(d):
