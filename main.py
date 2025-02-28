b06487509e54ed5791ef3ea9e9822214ca407aed
armyscoutrick <armyscoutrick@yahoo.com>
2025-02-27 20:09:36 -0500

    Update main.py

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ def main():
 
 def get_book_text(path):
     with open(path, 'r', encoding='utf-8') as f:
-        return f.read().replace('\r\n', '\n')
+        return f.read().strip()
 
 
 def print_report(book_path, num_words, chars_sorted_list):
