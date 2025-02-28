73b63d2ba42793df1542ae602e14566b77bcf4cf
armyscoutrick <armyscoutrick@yahoo.com>
2025-02-27 19:42:26 -0500

    Update stats.py

--- stats.py
+++ stats.py
@@ -1,4 +1,4 @@
 def get_num_words(text):
-    # Use the original method plus an adjustment to match the expected count
+    
     words = text.split()
-    return len(words) + 357  # The difference between 75410 and 75767
+    return len(words)
